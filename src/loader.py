6098b2360271d769adc65bd3288794dae99eefc0
Mike <daiy29@mcmaster.ca>
2022-11-13 00:08:40 -0500

    Renamed extract.py to loader.py, cleaned up file and added err check

---
+++ src/loader.py
@@ -0,0 +1,59 @@
+from riotwatcher import LolWatcher, ApiError
+import requests
+from dotenv import load_dotenv
+import logging
+import os
+
+load_dotenv()
+api_key = os.environ.get('API_KEY')
+my_region = os.environ.get('REGION')
+player_list = os.environ.get('PLAYERLIST')
+certificate = os.environ.get('CERTIFICATE')
+
+lol_watcher = LolWatcher(api_key)
+
+try:
+    url = requests.get(player_list, verify=certificate) 
+except Exception as e:
+    logging.exception("API call has failed")
+
+gamedata = url.json()
+
+def requestSummonerTeam(data):
+    for summoner in data:
+        team = summoner["team"]
+    return team
+
+def requestSummonerName(data):
+    for summoner in data:
+        summoner_name = summoner["summonerName"]
+    return summoner_name
+
+def requestSummonerId(region): #hardcoded my shit for testing, need to change later. Maybe should move api request outside of method.
+    try:
+        data = lol_watcher.summoner.by_name(region, 'Legendai') #if using api, can change to use api error method
+    except Exception as e:
+        logging.exception("API call has failed")
+    summoner_id = data["id"]
+    return summoner_id
+
+def requestSpellId(region, summonerid):
+    try:
+        data = lol_watcher.spectator.by_summoner(region, summonerid)
+    except Exception as e:
+        logging.exception("API call has failed")
+    spell1_id = data["participants"][0]["spell1Id"]
+    spell2_id = data["participants"][0]["spell2Id"]
+    return ([spell1_id,spell2_id])
+
+def requestRuneId(region, summonerid):
+    try:
+        data = lol_watcher.spectator.by_summoner(region, summonerid)
+    except Exception as e:
+        logging.exception("API call has failed")
+    rune_id = data["participants"][0]["perks"]
+    return (rune_id)
+
+# need something that parses perk ids to actual rune, but this is sufficient for now
+
+#add check for not in game maybe?, add class for runes, summoner spells??
